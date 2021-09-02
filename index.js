081440c9b2c186aa27d22a0b72ce5c02be7fc82e
Subramanya G <subramanyag9112@gmail.com>
2021-09-02 13:21:03 +0530

    Add payment and mailing code

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const userRoutes = require('./routes/userRoutes.js');
 const companyRoutes = require('./routes/companyRoutes.js');
 const { notFound, errorHandler } = require('./middleware/error.js');
 const multer = require('multer');
-// const cors = require('cors');
+const cors = require('cors');
+const shortid = require('shortid')
+const Razorpay = require('razorpay')
+const bodyParser = require('body-parser')
+
+const razorpay = new Razorpay({
+    key_id: 'rzp_test_fojLAXlUSy8SKB',
+    key_secret: '7LKPfIcR9tbtwwyWAouhVJf0'
+});
+
 // const bodyParser = require('body-parser');
 
+require('dotenv').config()
 
 const app = express()
-// app.use(cors)
+app.use(cors())
+app.use(bodyParser.json())
 
 //connect DB
 connectDB()
@@ -27,20 +38,20 @@ const fileStorage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-) {
-    cb(null, true);
-} else {
-    cb(null, false);
-}
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
 };
 
 // middleware for req.body to parse 
 app.use(express.json())
-app.use(multer({ storage : fileStorage, fileFilter : fileFilter }).single('image'));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 
 // middleware - access to any req-res cycle
 app.use((req, res, next) => {
@@ -51,7 +62,7 @@ app.use((req, res, next) => {
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use( express.static(path.join(__dirname, 'logo')));
+app.use(express.static(path.join(__dirname, 'logo')));
 app.use('/docs', express.static(path.join(__dirname, 'docs')));
 
 
@@ -59,6 +70,75 @@ app.use('/docs', express.static(path.join(__dirname, 'docs')));
 app.use('/api/users', userRoutes)
 app.use('/api/company', companyRoutes)
 
+app.get('/lg.png', (req, res) => {
+    res.sendFile(path.join(__dirname, 'logo', '1630507769181-lg.png'))
+})
+
+app.post('/verification', (req, res) => {
+    const secret = 'abcdefghijklmnopqrstuvwxyz'
+
+    const crypto = require('crypto')
+    const shasum = crypto.createHmac('sha256', secret)
+    shasum.update(JSON.stringify(req.body))
+    const digest = shasum.digest('hex')
+
+    if (digest === req.headers['x-razorpay-signature']) {
+        console.log(req.body);
+        console.log(req.body['payload']['payment']);
+        console.log('request is legit')
+        // require('fs').writeFileSync('payment1.json', JSON.stringify(req.body, null, 4))
+    } else {
+        // pass it
+    }
+    res.json({ status: 'ok' })
+})
+
+app.post('/razorpay', async (req, res) => {
+    const payment_capture = 1
+    const amount = 499
+    const currency = 'INR'
+
+    const options = {
+        amount: amount * 100,
+        currency,
+        receipt: shortid.generate(),
+        payment_capture
+    }
+
+    try {
+        const response = await razorpay.orders.create(options)
+        console.log(response)
+        res.json({
+            id: response.id,
+            currency: response.currency,
+            amount: response.amount
+        })
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+
+function sendmail(to, subject, html) {
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+        to: to,
+        from: 'subramanyag9112@gmail.com',
+        subject: subject,
+        text: '',
+        html: html,
+    }
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent')
+            res.send("OKAY");
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+}
 
 // fallback for 404 error (using after all routes)
 app.use(notFound)
